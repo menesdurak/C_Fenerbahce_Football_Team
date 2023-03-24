eb8adb033f0a0dd6a6355b600a65cd8f6747102e
menesdurak <m.enes.durak@gmail.com>
2023-03-24 10:33:16 +0300

    Created player list

--- app/src/main/java/com/menesdurak/fenerbahce_football_team/data/Player.kt
+++ app/src/main/java/com/menesdurak/fenerbahce_football_team/data/Player.kt
@@ -2,6 +2,7 @@ package com.menesdurak.fenerbahce_football_team.data
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import com.menesdurak.fenerbahce_football_team.R
 
 data class Player(
     @DrawableRes val imageResourceId: Int,
@@ -9,3 +10,19 @@ data class Player(
     val age: Int,
     @StringRes val position: Int
 )
+
+val players = listOf(
+    Player(R.drawable.altay_bayindir, R.string.altay_bayindir, 25, R.string.goalkeeper),
+    Player(R.drawable.lincoln_henrique, R.string.lincoln_henrique, 25, R.string.defence),
+    Player(R.drawable.attila_szalai, R.string.attila_szalai, 25, R.string.defence),
+    Player(R.drawable.samet_akaydin, R.string.samet_akaydin, 25, R.string.defence),
+    Player(R.drawable.osayi_samuel, R.string.brigh_osayi_samuel, 25, R.string.defence),
+    Player(R.drawable.arao, R.string.willian_arao, 25, R.string.midfielder),
+    Player(R.drawable.ismail_y_ksek, R.string.ismail_yuksek, 25, R.string.midfielder),
+    Player(R.drawable.arda_guler, R.string.arda_guler, 25, R.string.midfielder),
+    Player(R.drawable.diego_rossi, R.string.diego_rossi, 25, R.string.forward),
+    Player(R.drawable.emre_mor, R.string.emre_mor, 25, R.string.forward),
+    Player(R.drawable.enner_valencia, R.string.enner_valencia, 25, R.string.forward)
+)
+
+
